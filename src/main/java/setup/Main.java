fe4345b57ea4836664d7d39c740be18b8aa59269
followyourdream1337 <119725883+followyourdream1337@users.noreply.github.com>
2026-03-11 12:08:55 +0000

    init

--- src/main/java/setup/Main.java
+++ src/main/java/setup/Main.java
@@ -1,19 +1,35 @@
 package setup;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.bidi.module.Input;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
 
 public class Main {
-
-
+  static protected WebDriver driver;
   static {
-
+    chromedriver().setup();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    driver = new ChromeDriver(options);
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
+    driver.get("https://www.selenium.dev/selenium/web/formPage.html");
 
+    System.out.println("Title: " + driver.getTitle());
+    // WebElement element =
+    // driver.findElement(By.xpath("//input[@value='Click!']"));
+    // Thread.sleep(5000);
+    // element.click();
+    WebElement inputElement = driver.findElement(By.id("inputWithText"));
+    inputElement.clear();
+    inputElement.sendKeys("enes");
+    Thread.sleep(6000);
+    // driver.quit();
   }
 }
